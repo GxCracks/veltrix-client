6845380a17a734b327896c41632648c7167d0a69
GxCracks <fakeagentbanane@gmail.com>
2026-09-16 23:10:18 +0200

    fix: point Windows download to VELTRIX 0.8.2

--- script.js
+++ script.js
@@ -41,7 +41,7 @@
     .then(data => renderNews(data.items))
     .catch(() => {});
 
-  const windowsInstallerUrl = 'https://github.com/GxCracks/veltrix-client/releases/download/v0.8.1/VELTRIX-Setup-0.8.1.exe';
+  const windowsInstallerUrl = 'https://github.com/GxCracks/veltrix-client/releases/download/v0.8.2/VELTRIX-Setup-0.8.2.exe';
   const windowsDownload = document.querySelector('[data-platform="windows"] .platform-action');
   if (windowsDownload) {
     windowsDownload.setAttribute('href', windowsInstallerUrl);
